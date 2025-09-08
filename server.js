f7b713c72e78cc6e50360493d78fde1aad6ebfd8
Arsalan Ali <arsalanali124000@gmail.com>
2025-09-08 19:10:43 +0500

    remove compression from server.js

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use(cors({
 }));
 app.use(express.json({ limit: "10mb" }));
 app.use(helmet());
-app.use(compression());
 app.use(morgan("dev"));
 app.use('/webhook', require('./routes/callRoutes.js'));
 app.use('/api/stats', require('./routes/stats'));        
