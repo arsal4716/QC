093e2a1e1a344471c1021e15d0910c49ff1d81fe
Arsalan Ali <arsalanali124000@gmail.com>
2025-11-19 17:58:51 +0500

    add caps route

--- routes/capsRoutes.js
+++ routes/capsRoutes.js
@@ -2,13 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { 
   getCaps, 
-  fetchAndSaveCaps, 
   updateTarget, 
   processPixelFire 
 } = require('../controllers/capsController');
 
 router.get('/', getCaps);
-router.post('/fetch', fetchAndSaveCaps);
 router.patch('/target/:id', updateTarget);
 router.get('/pixel', processPixelFire); 
 
